de181f73a7da6131d299caa3f43109ec03e44d60
Jonathan Troiano <33912011+jjtroiano0272@users.noreply.github.com>
2021-06-22 14:43:53 -0400

    Roll back to previous working version. Ugly organization but I don't understand why it won't work when I actually subdivide the paths, so.. 🤷‍♂️

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /* OrbitControls allows us to pan around the scene with our controller (mouse/finger) */
 import './style.css';
 import * as THREE from 'three';
+// import * as THREE from './vendor/three/build/three.module.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Think of a scene as a container
