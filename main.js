5bb74106170e0944710ed653f6459317ae16bf4a
Jonathan Troiano <33912011+jjtroiano0272@users.noreply.github.com>
2021-07-02 15:49:39 -0400

    Fixes aspect ratio resize issues & mobile layout

--- main.js
+++ main.js
@@ -12,22 +12,23 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
-}
+};
 
 /* Boilerplate stuff */
-window.addEventListener('resize', () => {
-    // Update sizes
+// FIXME: Where did I get this code?
+/* window.addEventListener('resize', () => {
+    Update sizes
     sizes.width = window.innerWidth;
     sizes.height = window.innerHeight;
 
-    // Update camera
+    Update camera
     camera.aspect = sizes.width / sizes.height;
     camera.updateProjectionMatrix();
     
-    // Update renderer
+    Update renderer
     renderer.setSize(sizes.width, sizes.height);
-    render.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-})
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+}); */
 
 /* This was all math until now. The renderer object renders the actual graphics to see.
 renderer needs to know which DOM elements to use to render graphics upon */
@@ -35,17 +36,13 @@ const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('#bg')
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
-renderer.render(scene, camera);
+// renderer.render(scene, camera);
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-/* Most materials require a light source to bounce off of them to be visible.
-In this case, MeshBasicMaterial doesn't need one! */
-const material = new THREE.MeshStandardMaterial({ color: 0xFF6347 });
 // We must create a mesh by combining the geometry with the material.
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0xFF6347 });
 const torus = new THREE.Mesh(geometry, material);
 
-// Need to add the defined mesh (geometry + mesh) to the scene then...
-// VIDEO: 06:20
 scene.add(torus);
 
 const pointLight = new THREE.PointLight(0xFFFFF);
@@ -53,13 +50,15 @@ pointLight.position.set(5,5, 5);
 const ambientLight = new THREE.AmbientLight(0xFFFFFF);
 scene.add(pointLight, ambientLight);
 
-// Adds indicators for each, which are normally hidden!
+/* *** DEBUG TOOLS ***
+********************** Adds indicators for each, which are normally hidden! */
 // const lightHelper = new THREE.PointLightHelper(pointLight);
 // const gridHelper = new THREE.GridHelper(200, 50);
 // scene.add(lightHelper, gridHelper);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
+// Add 200 stars & set an external image as background
 function addStar () {
     const geometry = new THREE.SphereGeometry(0.25, 24, 24);
     const material = new THREE.MeshStandardMaterial({ color: 0xFFFFFF });
@@ -69,13 +68,12 @@ function addStar () {
     star.position.set(x, y, z);
     scene.add(star);
 }
-// Add 200 stars & set an external image as background
 Array(200).fill().forEach(addStar);
 
 const spaceTexture = new THREE.TextureLoader().load('./img/space.jpg');
 scene.background = spaceTexture;
 
-const profileCubeTexture = new THREE.TextureLoader().load('./img/IMG_7268.jpg');
+const profileCubeTexture = new THREE.TextureLoader().load('./img/profile_pic_stylized.jpg');
 const profileCube = new THREE.Mesh(
     new THREE.BoxGeometry(3, 3, 3),
     new THREE.MeshBasicMaterial({ map: profileCubeTexture })
@@ -98,7 +96,6 @@ scene.add(moon);
 moon.position.z = 30;
 moon.position.setX(-10);
 
-
 // Animations that happen when you scroll
 function moveCamera() {
     const t = document.body.getBoundingClientRect().top;
@@ -120,20 +117,25 @@ moveCamera();
 
 // Recursive animation loop, works like a Game Loop
 function animate() {
-    requestAnimationFrame(animate);
     torus.rotation.x += 0.010;
     torus.rotation.y += 0.005;
     torus.rotation.z += 0.010;
-
+    
     // profile cube slowly rotates by default
     profileCube.rotation.x -= 0.00040;
     profileCube.rotation.y += 0.00008;
-
+    
     moon.rotation.x += 0.005;
     moon.rotation.y += 0.003;
-
+    
     // So whenever the browser repaints the screen it calls the render method to update UI
+    const canvas = renderer.domElement;
+    // camera.aspect = document.querySelector('#bg').clientWidth / document.querySelector('#bg').clientHeight;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+    
     renderer.render(scene, camera);
+    requestAnimationFrame(animate);
 }
 
 // Entry point
