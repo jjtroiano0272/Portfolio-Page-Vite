e450a04a7778fb70929640620237fd314e297ff2
Jonathan Troiano <33912011+jjtroiano0272@users.noreply.github.com>
2021-07-28 13:19:35 -0400

    Adds subpage for drafting vaporwave aesthetic

--- INFINITE_TERRAIN_Johan.js
+++ INFINITE_TERRAIN_Johan.js
@@ -1,11 +1,14 @@
 /*
-  Johan Karlsson (DonKarlssonSan) 2018
+  Credit for the original implementation of code: Johan Karlsson (DonKarlssonSan) 2018
   https://codepen.io/DonKarlssonSan/pen/deVYoZ
-  Referencing: 
-   * three.js
-   * Noise lib
-   * OrbitControls
+
+  That original code does NOT work with current-day Three.js and had to be translated to current methods.
 */
+// 0x461e52
+// 0xdd517f
+// 0xe68e36
+// 0x556dc8
+// 0x7998ee
 
 import './styles.css';
 import * as THREE from 'three';
@@ -19,12 +22,15 @@ let scene;
 let camera;
 let renderer;
 let simplex;
+let sides;
 let planeMesh;
 let geometry;
 let xZoom;
 let yZoom;
 let noiseStrength;
 let offset;
+let planeVertices;
+let flying = 0;
 
 function setup() {
   setupNoise();
@@ -37,8 +43,7 @@ function setup() {
 }
 
 function setupNoise() {
-  // By zooming y more than x, we get the
-  // appearence of flying along a valley
+  // By zooming y more than x, we get the appearence of flying along a valley
   xZoom = 6;
   yZoom = 18;
   noiseStrength = 1.5;
@@ -68,12 +73,21 @@ function setupCamera() {
   let controls = new OrbitControls(camera, renderer.domElement);
 }
 
+// TODO:
+function setupFog() {
+  scene.fog = new THREE.FogExp2( 0x461e52, 0.14 );
+  renderer.setClearColor(0x461e52, 1);
+}
+
 function setupPlaneMesh() {
-  let side = 32;
-  geometry = new THREE.PlaneGeometry(40, 40, side, side);
+  sides = Math.pow(2, 5);
+  // sides = 1;
+  geometry = new THREE.PlaneGeometry(40, 40, sides, sides);
+
+  // TODO: geometry.parameters.widthSegments
 
   let material = new THREE.MeshStandardMaterial({
-    roughness: 0.8,
+    roughness: 0.9,
     color: new THREE.Color(0x556DC8),
     wireframe: true
   });
@@ -85,6 +99,15 @@ function setupPlaneMesh() {
   scene.add(planeMesh);
 }
 
+function setupSun() {
+  let geometry = new THREE.SphereGeometry( 5, 32, 32 );
+  let material = new THREE.MeshBasicMaterial({ color: 0xe68e36 } );
+  let sun = new THREE.Mesh( geometry, material );
+  scene.add(sun);
+}
+
+function setupStars() {}
+
 function setupLights() {
   let ambientLight = new THREE.AmbientLight(0x0c0c0c);
   scene.add(ambientLight);
@@ -92,6 +115,14 @@ function setupLights() {
   let spotLight = new THREE.SpotLight(0xcccccc);
   spotLight.position.set(-30, 60, 60);
   spotLight.castShadow = true;
+
+    var hemisphereLight = new THREE.HemisphereLight(0xe68e36,0xdd517f, .9)
+    scene.add(hemisphereLight);
+    let sun = new THREE.DirectionalLight(0xe68e36, 0.9);
+    sun.position.set( 12, 6, -7 );
+    sun.castShadow = true;
+    scene.add(sun);
+
   scene.add(spotLight);
 }
 
@@ -105,22 +136,34 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function draw() {
-  requestAnimationFrame(draw);
-  offset = Date.now() * 0.0004;
-  adjustVertices(offset);
-	adjustCameraPos(offset);
-  renderer.render(scene, camera);
-}
-
-function adjustVertices(offset) {
-    let planeVertices = planeMesh.geometry.attributes.position;
+function undulateVertices(offset) {
+    planeVertices = planeMesh.geometry.attributes.position; // Float32Array: 1-dimensional Array
+    let numRows = planeVertices.count / sides+1;
     
     for (let i = 0; i < planeVertices.count; i++) {
         let x = planeVertices.getX(i) / xZoom;
         let y = planeVertices.getY(i) / yZoom;
-        let noise = simplex.noise2D(x, y + offset) * noiseStrength;
+        let noise = simplex.noise2D(x, y+offset) * noiseStrength;
         planeVertices.setZ(i, noise);
+
+        // TODO: x coordinates of the planeMesh increase at a linear rate, towards camera
+        let gridDisplacement = -0.120;
+        planeVertices.setY( i, planeVertices.getY(i)+gridDisplacement );
+
+        if (planeVertices.getY(i) < camera.position.y ) {
+          
+        }
+
+        for (let row = 0; row < numRows; row++) {
+          planeVertices.getY(row);
+          
+        }
+        
+        // let numRows;
+        // for (let col = 0; col < planeVertices.count; col++) {
+        //   numRows++;
+        //   console.log(numRows);
+        // }
     }
     
     planeVertices.needsUpdate = true;
@@ -133,6 +176,35 @@ function adjustCameraPos(offset) {
   let noise = simplex.noise2D(x, y+offset) * noiseStrength + 1.5; 
   camera.position.z = noise;
 }
+
+function proceduralGen() {
+  planeVertices = planeMesh.geometry.attributes.position;
+  let numRows = planeVertices.count / sides+1;
+
+  for (let col = 0; col < numRows; col++) { // iterate over columns
+    if ( planeVertices.getY(col) === planeVertices.getY(col++)  ) { // They're in same row; iterate over rows
+      let x = planeVertices.getX(col) / xZoom;
+      let y = planeVertices.getY(col) / yZoom;
+      let noise = simplex.noise2D(x, y+offset) * noiseStrength;
+      planeVertices.setZ(col, noise);
+
+      // planeVertices.setY(0, )
+    }
+    
+  }
+}
+
+function draw() {
+  requestAnimationFrame(draw);
+  offset = Date.now() * 0.0004;
+  undulateVertices(offset);   // wiggles the geometry
+	adjustCameraPos(offset);    // dolly's the camera up & down
+  proceduralGen();
+
+  renderer.render(scene, camera);
+}
+
+
 
 
 
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Portfolio: Jonathan Troiano
- This will contain the documentation of the process of creating this page. 
- Short, maybe with a few gifs.
+
+This will contain the documentation of the process of creating this page.
+Short, maybe with a few gifs.
+
+# INFINITE_TERRAIN_Johan
+
+- To make the terrain bigger, multiply all parameters of PlaneGeometry constructor `geometry = new THREE.PlaneGeometry(40, 40, side, side);` by a scale factor.
+-
